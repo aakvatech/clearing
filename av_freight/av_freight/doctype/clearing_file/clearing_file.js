7660663cbcaed89e767ccefa4cad7ca6a4150e76
nelsonmpanju <nelsonnorbert87@gmail.com>
2024-07-22 11:52:47 +0300

    feat: Auto fetching of addresses and controll in document attachment

--- av_freight/av_freight/doctype/clearing_file/clearing_file.js
+++ av_freight/av_freight/doctype/clearing_file/clearing_file.js
@@ -6,8 +6,8 @@ frappe.ui.form.on('Clearing File', {
         frm.trigger('mode_of_transport');
     },
     refresh: function(frm) {
-        if (!frm.custom_buttons['Create Customs Clearence']) {
-            frm.add_custom_button(__('Create Customs Clearence'), function() {
+        if (!frm.custom_buttons['Create Customs Clearance']) {
+            frm.add_custom_button(__('Create Customs Clearance'), function() {
                 // Code to create Customs Clearance
                 frappe.call({
                     method: "frappe.client.insert",
@@ -30,7 +30,7 @@ frappe.ui.form.on('Clearing File', {
                 });
             }, 'Create');
         }
-        
+
         if (!frm.custom_buttons['Create Sales Order']) {
             frm.add_custom_button(__('Create Sales Order'), function() {
                 // Code to create Sales Order
@@ -39,98 +39,100 @@ frappe.ui.form.on('Clearing File', {
             }, 'Create');
         }
     },
-    customer: function(frm) {
-        if (frm.doc.customer) {
-            frappe.call({
-                method: "av_freight.av_freight.doctype.clearing_file.clearing_file.get_address_display_from_customer",
-                args: {
-                    "customer": frm.doc.customer
-                },
-                callback: function(r) {
-                    if (r.message) {
-                        frm.set_value('address_display', r.message.address_display);
-                        frm.set_value('customer_address', r.message.customer_address);
-                    } else {
-                        frm.set_value('address_display', '');
-                        frm.set_value('customer_address', '');
-                    }
+customer: function(frm) {
+    if (frm.doc.customer) {
+        frappe.call({
+            method: "av_freight.av_freight.doctype.clearing_file.clearing_file.get_address_display_from_link",
+            args: {
+                "doctype": "Customer",
+                "name": frm.doc.customer
+            },
+            callback: function(r) {
+                if (r.message) {
+                    frm.set_value('address_display', r.message.address_display);
+                    frm.set_value('customer_address', r.message.customer_address);
+                } else {
+                    frm.set_value('address_display', '');
+                    frm.set_value('customer_address', '');
                 }
-            });
-        } else {
-            frm.set_value('address_display', '');
-            frm.set_value('customer_address', '');
-        }
-    },
-    mode_of_transport: function(frm) {
-        frm.trigger('toggle_fields_based_on_transport');
-    },
-    carrier_name: function(frm) {
-        if (frm.doc.carrier_name && frm.doc.mode_of_transport) {
-            if (frm.doc.mode_of_transport == 'Sea') {
-                frm.fields_dict['voyage_flight_number'].get_query = function(doc) {
-                    return {
-                        filters: {
-                            'shipping_line': frm.doc.carrier_name
-                        }
-                    };
-                };
-            } else if (frm.doc.mode_of_transport == 'Air') {
-                frm.fields_dict['voyage_flight_number'].get_query = function(doc) {
-                    return {
-                        filters: {
-                            'airline': frm.doc.carrier_name
-                        }
-                    };
-                };
             }
-        } else {
-            frm.fields_dict['voyage_flight_number'].get_query = function(doc) {
-                return {
-                    filters: {}
-                };
-            };
-        }
-        frm.set_value('voyage_flight_number', '');
+        });
+    } else {
+        frm.set_value('address_display', '');
+        frm.set_value('customer_address', '');
+    }
+},
+shipper: function(frm){
+    if (frm.doc.shipper){
+        frappe.call({
+            method: "av_freight.av_freight.doctype.clearing_file.clearing_file.get_address_display_from_link",
+            args: {
+                "doctype": "Shipper",
+                "name": frm.doc.shipper
+            },
+            callback: function(r) {
+                if (r.message) {
+                    frm.set_value('shipper_address', r.message.address_display);
+                    frm.set_value('shipper_address_display', r.message.customer_address);
+                } else {
+                    frm.set_value('shipper_address_display', '');
+                    frm.set_value('shipper_address', '');
+                }
+            }
+
+        })
+    }
+},
+// customer_address: function(frm) {
+//     if (frm.doc.address_display) {
+//         // Assuming that address_display is manually filled in, set customer_address to address_display
+//         frm.set_value('address_display', frm.doc.address_display);
+//     }
+// },
+
+// shipper_address_display: function(frm) {
+//     if (frm.doc.shipper_address_display) {
+//         // Assuming that shipper_address_display is manually filled in, set shipper_address to shipper_address_display
+//         frm.set_value('shipper_address', frm.doc.shipper_address_display);
+//     }
+// },
+mode_of_transport: function(frm) {
+        frm.trigger('toggle_fields_based_on_transport');
     },
     toggle_fields_based_on_transport: function(frm) {
         if (frm.doc.mode_of_transport == 'Sea') {
-            frm.set_df_property('carrier_name', 'options', 'Shipping line');
-            frm.set_df_property('voyage_flight_number', 'options', 'Shipment Vessel');
             
             frm.toggle_display('carrier_name', true);
             frm.toggle_display('voyage_flight_number', true);
             frm.toggle_display('bill_of_lading_number', true);
-            frm.toggle_display('commercial_invoice_number', true);
+            
             
             // Hide fields related to Air transport
-            frm.toggle_display('flight_number', false);
             frm.toggle_display('airline', false);
-        } else if (frm.doc.mode_of_transport == 'Air') {
-            frm.set_df_property('carrier_name', 'options', 'Airline');
-            frm.set_df_property('voyage_flight_number', 'options', 'Airplane');
+            frm.toggle_display('airplane', false);
+            frm.toggle_display("air_waybill",false)
             
-            frm.toggle_display('carrier_name', true);
-            frm.toggle_display('voyage_flight_number', true);
-            
-            // Hide fields related to Sea transport
+        } else if (frm.doc.mode_of_transport == 'Air') { 
+            frm.toggle_display('carrier_name', false);
+            frm.toggle_display('voyage_flight_number', false); 
             frm.toggle_display('bill_of_lading_number', false);
-            frm.toggle_display('commercial_invoice_number', false);
-            frm.toggle_display('voyage_number', false);
-
+           
+            
             // Show fields related to Air transport
-            frm.toggle_display('flight_number', true);
             frm.toggle_display('airline', true);
-        } else {
-            frm.set_df_property('carrier_name', 'options', '');
-            frm.set_df_property('voyage_flight_number', 'options', '');
+            frm.toggle_display('airplane', true);
+            frm.toggle_display("air_waybill",true);
+
+        } else if (frm.doc.mode_of_transport == 'Land') {
+        
             
             // Hide all fields if mode of transport is not selected
             frm.toggle_display('carrier_name', false);
             frm.toggle_display('voyage_flight_number', false);
             frm.toggle_display('bill_of_lading_number', false);
-            frm.toggle_display('commercial_invoice_number', false);
-            frm.toggle_display('flight_number', false);
             frm.toggle_display('airline', false);
+            frm.toggle_display('airplane', false);
+            frm.toggle_display("air_waybill",false)
         }
         frm.set_value('carrier_name', '');
         frm.set_value('voyage_flight_number', '');
--- av_freight/av_freight/doctype/clearing_file/clearing_file.py
+++ av_freight/av_freight/doctype/clearing_file/clearing_file.py
@@ -6,18 +6,48 @@ from frappe.model.document import Document
 from frappe.contacts.doctype.address.address import get_address_display
 
 class ClearingFile(Document):
-    pass
+    def validate(self):
+        self.check_mandatory_fields_and_attachments()
+
+    def check_mandatory_fields_and_attachments(self):
+        mandatory_fields = [
+            'air_waybill',
+            'bill_of_lading_number',
+            'commercial_invoice_number'
+        ]
+        
+        # Mapping of field names to the document type they correspond to
+        field_to_doc_type = {
+            'air_waybill': 'Air Waybill',
+            'bill_of_lading_number': 'Bill of Lading Number',
+            'commercial_invoice_number': 'Commercial Invoice Number'
+        }
+
+        for field in mandatory_fields:
+            if getattr(self, field):
+                # Check if corresponding document is attached in the child table
+                attached = any(
+                    child.name1 == field_to_doc_type[field] and child.attachment
+                    for child in self.documents
+                )
+                if not attached:
+                    frappe.throw(f"Please attach the corresponding document for {field.replace('_', ' ').title()}.")
 
 @frappe.whitelist()
-def get_address_display_from_customer(customer):
-    if not customer:
+def get_address_display_from_link(doctype, name):
+    if not doctype or not name:
         return {"address_display": "", "customer_address": ""}
-    addresses = frappe.get_all('Address', filters={'link_doctype': 'Customer', 'link_name': customer}, fields=['name'])
+    
+    addresses = frappe.get_all('Address', filters={'link_doctype': doctype, 'link_name': name}, fields=['name'])
+    
     if not addresses:
         return {"address_display": "", "customer_address": ""}
+    
     address = frappe.get_doc("Address", addresses[0].name)
     address_display = get_address_display(address.as_dict())
+    
     return {"address_display": address_display, "customer_address": addresses[0].name}
+
 
 @frappe.whitelist()
 def get_dynamic_link_doctype(doctype, txt, searchfield, start, page_len, filters):
