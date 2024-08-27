4213b949dfa89c736b31ac0887d2c3770638ce94
nelsonmpanju <nelsonnorbert87@gmail.com>
2024-08-27 14:24:53 +0300

    feat: adding features

--- clearing/clearing/doctype/clearing_file/clearing_file.py
+++ clearing/clearing/doctype/clearing_file/clearing_file.py
@@ -4,9 +4,34 @@
 import frappe
 from frappe.model.document import Document
 from frappe.contacts.doctype.address.address import get_address_display
+from frappe import _
 
 class ClearingFile(Document):
-    pass
+    def validate(self):
+        self.check_and_update_status()
+
+    def check_and_update_status(self):
+        # Required documents for "Pre-Lodged" status
+        required_docs = ['Packing List', 'Commercial Invoice']
+
+        # Depending on the mode of transport, check for the specific document
+        if self.mode_of_transport == 'Sea':
+            required_docs.append('Bill of Lading B/L')
+        elif self.mode_of_transport == 'Air':
+            required_docs.append('Air Waybill (AWB)')
+
+        # Check if each required document is present in the Clearing File's child table 'documents'
+        missing_docs = []
+        for doc_name in required_docs:
+            exists = any(doc.document_name == doc_name for doc in self.document)
+            if not exists:
+                missing_docs.append(doc_name)
+
+        # If no documents are missing, update the status to "Pre-Lodged"
+        if not missing_docs:
+            self.status = 'Pre-Lodged'
+        else:
+           pass
 
 
 
--- clearing/clearing/doctype/tra_clearance/tra_clearance.py
+++ clearing/clearing/doctype/tra_clearance/tra_clearance.py
@@ -1,8 +1,38 @@
 # Copyright (c) 2024, Nelson Mpanju and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe import _
 
 class TRAClearance(Document):
-    pass
+    def validate(self):
+        self.check_required_documents()
+
+    def check_required_documents(self):
+        # Fetch the related Clearing File document
+        clearing_file_doc = frappe.get_doc('Clearing File', self.clearing_file)
+
+        # Required documents
+        required_docs = ['Packing List', 'Commercial Invoice']
+
+        # Check if each required document is present in the Clearing File's child table 'documents'
+        missing_docs = []
+        for doc_name in required_docs:
+            exists = any(doc.document_name == doc_name for doc in clearing_file_doc.document)
+            if not exists:
+                missing_docs.append(doc_name)
+
+        # Check if either "Air Waybill (AWB)" or "Bill of Lading B/L" is present
+        if clearing_file_doc.mode_of_transport == 'Sea':
+            has_bill_of_lading = any(doc.document_name == 'Bill of Lading B/L' for doc in clearing_file_doc.document)
+            if not has_bill_of_lading:
+                missing_docs.append('Bill of Lading B/L')
+        elif clearing_file_doc.mode_of_transport == 'Air':
+            has_air_waybill = any(doc.document_name == 'Air Waybill (AWB)' for doc in clearing_file_doc.document)
+            if not has_air_waybill:
+                missing_docs.append('Air Waybill (AWB)')
+
+        # If any required document is missing, raise a validation error
+        if missing_docs:
+            frappe.throw(_('The following documents are required before creating TRA Clearance: {0}').format(', '.join(missing_docs)))
